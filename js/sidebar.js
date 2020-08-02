1115445d3f070f96a5fe21f52179b832c8d56ade
souvenchen <s1927736@sms.ed.ac.uk>
2020-08-02 05:40:37 +0300

    01/08/2020

---
+++ js/sidebar.js
@@ -0,0 +1,50 @@
+function HideOrShowFont(obj) {
+    if (obj == 'a1') {
+        document.getElementById("a1").style.color = "#2577e3"; 
+        document.getElementById("a2").style.color = "#333";
+        document.getElementById("a3").style.color = "#333";
+        document.getElementById("a4").style.color = "#333"; 
+        document.getElementById("a5").style.color = "#333";
+        document.getElementById("a6").style.color = "#333";
+    }
+   else if (obj == 'a2') {
+        document.getElementById("a1").style.color = "#333"; 
+        document.getElementById("a2").style.color = "#2577e3";
+        document.getElementById("a3").style.color = "#333";
+        document.getElementById("a4").style.color = "#333"; 
+        document.getElementById("a5").style.color = "#333";
+        document.getElementById("a6").style.color = "#333";
+    }
+    else if (obj == 'a3') {
+        document.getElementById("a1").style.color = "#333"; 
+        document.getElementById("a2").style.color = "#333";
+        document.getElementById("a3").style.color = "#2577e3";
+        document.getElementById("a4").style.color = "#333"; 
+        document.getElementById("a5").style.color = "#333";
+        document.getElementById("a6").style.color = "#333";
+    }
+    else if (obj == 'a4') {
+        document.getElementById("a1").style.color = "#333"; 
+        document.getElementById("a2").style.color = "#333";
+        document.getElementById("a3").style.color = "#333";
+        document.getElementById("a4").style.color = "#2577e3"; 
+        document.getElementById("a5").style.color = "#333";
+        document.getElementById("a6").style.color = "#333";
+    }
+    else if (obj == 'a5') {
+        document.getElementById("a1").style.color = "#333"; 
+        document.getElementById("a2").style.color = "#333";
+        document.getElementById("a3").style.color = "#333";
+        document.getElementById("a4").style.color = "#333"; 
+        document.getElementById("a5").style.color = "#2577e3";
+        document.getElementById("a6").style.color = "#333";
+    }
+    else if (obj == 'a6') {
+        document.getElementById("a1").style.color = "#333"; 
+        document.getElementById("a2").style.color = "#333";
+        document.getElementById("a3").style.color = "#333";
+        document.getElementById("a4").style.color = "#333"; 
+        document.getElementById("a5").style.color = "#333";
+        document.getElementById("a6").style.color = "#2577e3";
+    }
+}  
